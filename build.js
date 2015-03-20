322d73cec30b6ee9e9ab9fe1dab01dbd42ed9e14
Tom MacWright <tom@macwright.org>
2015-03-20 15:28:45 -0400

    Fixup build script

--- build.js
+++ build.js
@@ -3,21 +3,22 @@ var request = require('request'),
     zipfile = require('zipfile'),
     fs = require('fs');
 
+try { fs.unlinkSync('swot.zip'); } catch(e) { }
 console.error('downloading swot data');
 var req = request('https://codeload.github.com/leereilly/swot/zip/master');
 req.pipe(fs.createWriteStream('swot.zip'));
 req.on('end', downloaded);
 
-// downloaded();
-
 function downloaded() {
+    console.log('download complete');
+    console.log('unzipping & mushing');
     var zf = new zipfile.ZipFile('swot.zip');
     var bar = new ProgressBar(':bar :percent | :current/:total', {
         total: zf.names.length
     });
     var reduced = zf.names.filter(function(name) {
         return name.match(/swot-master\/lib\/domains\/.*.txt/);
-    }).reduce(function(memo, name, i) {
+    }).reduce(function(memo, name) {
         var reduced_name = name.replace(/^swot-master\/lib\/domains\//, '')
             .replace(/\.txt$/, '');
         var label = zf.readFileSync(name);
@@ -27,4 +28,10 @@ function downloaded() {
         return memo;
     }, {});
     fs.writeFileSync('swot-data.json', JSON.stringify(reduced, null, 2));
+    distill();
+}
+
+function distill() {
+    var data = require('./swot-data.json');
+    fs.writeFileSync('swot-data-bool.json', JSON.stringify(Object.keys(data)));
 }
